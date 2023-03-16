b16ce343593c96a4e1867736b4fc7808d51400b2
seraGatda <sera.lee@gatda.com>
2023-03-16 11:09:20 +0900

    Feat : UI 동작, json 파일, 애니메이션 추가
    UI 동작, json 파일, 애니메이션 추가

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
-AOS.init();
+// Global settings:
+AOS.init({
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: "DOMContentLoaded", // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: "aos-init", // class applied after initialization
+  animatedClassName: "aos-animate", // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 0, // offset (in px) from the original trigger point
+  delay: 50, // values from 0 to 3000, with step 50ms
+  duration: 500, // values from 0 to 3000, with step 50ms
+  easing: "ease-in-out-cubic", // default easing for AOS animations
+  once: true, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
+});
+
+//FAQ 클릭 시 클래스 토글
+const FaqList = document.getElementById("faqList");
+
+const toggleFaq = () => {
+  const toggleWrap = event.target.parentElement.parentElement;
+  const answerBox = toggleWrap.parentElement.nextElementSibling;
+
+  toggleWrap.classList.toggle("open");
+  answerBox.classList.toggle("hidden");
+};
